6c28321736e5e80823e9356f18c43ed92f6344b8
Chaziz <chazpelo2@gmail.com>
2025-07-24 02:44:29 -0400

    the

---
+++ CoreUtilities.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+  OpenSB: The Open SquareBracket Software
+
+  Copyright (C) 2025 Chaziz
+
+  OpenSB is free software: you can redistribute it and/or modify it under the 
+  terms of the GNU Affero General Public License as published by the Free 
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version. 
+
+  OpenSB is distributed in the hope that it will be useful, but WITHOUT ANY 
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more 
+  details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace BluffingoCore;
+
+/**
+ * Static core utilities.
+ */
+class CoreUtilities
+{
+    public static function getURL(bool $includeURI = false): ?string
+    {
+        if (!isset($_SERVER['HTTP_HOST'])) {
+            return null;
+        }
+
+        $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+        $host = $_SERVER['HTTP_HOST'];
+
+        if ($includeURI && isset($_SERVER['REQUEST_URI'])) {
+            return $protocol . '://' . $host . $_SERVER['REQUEST_URI'];
+        }
+
+        return $protocol . '://' . $host;
+    }
+}
