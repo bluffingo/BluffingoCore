1c9dfeba997157c60d7c1af2b3ab5c87312c031b
Chaziz <chazpelo2@gmail.com>
2025-10-24 08:10:53 -0400

    move iplookup and localization from opensb, add (incomplete) branding class

---
+++ Branding.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+  BluffingoCore
+
+  Copyright (C) 2025 Chaziz
+
+  BluffingoCore is free software: you can redistribute it and/or modify it 
+  under the terms of the GNU Affero General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or (at 
+  your option) any later version. 
+
+  BluffingoCore is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more 
+  details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace BluffingoCore;
+
+/**
+ * class Branding
+ */
+class Branding
+{
+    private $brand_settings = [
+        "name" => "BrandName",
+        "assets" => "/assets/qb_branding",
+        "is_vector" => true,
+        "use_wordmark" => false,
+    ];
+
+    public function __construct() {}
+
+    public function getBrandSettings()
+    {
+        return $this->brand_settings;
+    }
+}
